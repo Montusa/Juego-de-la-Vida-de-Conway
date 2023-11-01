6cc0e47e008480dfbb4af200564466b8a3333dc2
Facundo Monti <facundomonti03@gmail.com>
2023-11-01 15:34:13 -0300

    commentario 're loco' para probar github

--- Interfaz.java
+++ Interfaz.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
+//re loco
 public class Interfaz {
     private JFrame frame;
     private Tablero tablero;
--- Tablero.java
+++ Tablero.java
@@ -1,3 +1,32 @@
 public class Tablero {
-    
+    private int tamanio;
+    private Celula[][] mundo = new Celula[tamanio][tamanio];
+
+    Tablero(int tamanio){
+        this.tamanio = tamanio;
+    }
+
+    public void imprimir(){
+
+    }
+
+    public Celula[][] getMundo() {
+        return mundo;
+    }
+
+    public int cantVecinos(Celula cell){
+        return cell.getVecinos();
+    }
+
+    public void crearMundo(int cant){
+        for(int i=0; i<=tamanio; i++){
+            for(int j=0; j<=tamanio; j++){
+                int[] pos = new int[2];
+                pos[0] = i;
+                pos[1] = j;
+                Celula cell = new Celula(pos);
+                mundo[i][j] = cell;
+            }
+        }
+    }
 }
